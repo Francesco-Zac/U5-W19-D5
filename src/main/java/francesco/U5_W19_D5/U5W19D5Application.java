e06219059c59ff0bd3fc450d4ac77948c1301866
Francesco-Zac <francescozaccaro2@gmail.com>
2025-08-01 11:59:25 +0200

    first

---
+++ src/main/java/francesco/U5_W19_D5/U5W19D5Application.java
@@ -0,0 +1,13 @@
+package francesco.U5_W19_D5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U5W19D5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U5W19D5Application.class, args);
+	}
+
+}
---
+++ src/test/java/francesco/U5_W19_D5/U5W19D5ApplicationTests.java
@@ -0,0 +1,13 @@
+package francesco.U5_W19_D5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5W19D5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
