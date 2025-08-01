eeb8793dbec465c9129484869758b8882c040926
Francesco-Zac <francescozaccaro2@gmail.com>
2025-08-01 12:13:48 +0200

    create model

---
+++ src/main/java/francesco/U5_W19_D5/model/Booking.java
@@ -0,0 +1,41 @@
+package francesco.U5_W19_D5.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Booking {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private User user;
+
+    @ManyToOne
+    private Event event;
+
+    public Long getId() {
+        return id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public Event getEvent() {
+        return event;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public void setEvent(Event event) {
+        this.event = event;
+    }
+}
---
+++ src/main/java/francesco/U5_W19_D5/model/Event.java
@@ -0,0 +1,77 @@
+package francesco.U5_W19_D5.model;
+
+import jakarta.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+public class Event {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String description;
+    private LocalDate date;
+    private String location;
+    private int availableSeats;
+
+    @ManyToOne
+    private User organizer;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public int getAvailableSeats() {
+        return availableSeats;
+    }
+
+    public User getOrganizer() {
+        return organizer;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public void setAvailableSeats(int availableSeats) {
+        this.availableSeats = availableSeats;
+    }
+
+    public void setOrganizer(User organizer) {
+        this.organizer = organizer;
+    }
+}
